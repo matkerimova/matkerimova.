6c60581c363eb8d2f2b8a93d9801cd41d52ccba2
kosmosbekov21 <sanjarkachkynbaev.gmail.com>
2022-05-17 19:50:50 +0600

    pagess

---
+++ script.js
@@ -0,0 +1,14 @@
+let button = document.getElementById('btn')
+button.addEventListener('dblclick', func)
+
+function func() {
+    alert('you cliced me')
+}
+let butt = document.querySelector('.plus')
+butt.addEventListener('click', func)
+let p = document.getElementById('on')
+function func() {
+    p.style.display = 'none'
+    
+}
+
